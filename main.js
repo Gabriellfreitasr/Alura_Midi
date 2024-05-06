f9039c9d6350f3157c4285aedffd419cb6a25617
Gabriellfreitasr <gabriellfreitas456@gmail.com>
2024-05-06 20:46:56 -0300

    Otimizando...

--- main.js
+++ main.js
@@ -4,20 +4,13 @@ function tocaSom (idElementoSom) {
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-let contador = 0;
-
-// Enquanto = While é uma estrutura de repetição, usada para diminuir a repetição de códigos.
-while (contador < listaDeTeclas.length) {
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 
     const tecla = listaDeTeclas[contador];
     const instrumento = tecla.classList[1];
-
     const idAudio = `#som_${instrumento}`;
 
     tecla.onclick = function () {
         tocaSom(idAudio);
     }
-
-    contador ++;    
-
 } 
